920d51aa680a0a2356fae8e78d21f76701746b82
josh <61042416+joshuafirme@users.noreply.github.com>
2021-11-06 07:33:04 +0800

    Initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sku',
+        'price',
+        'description',
+        'features',
+        'directions_and_precautions',
+        'ingredients',
+        'category_id',
+        'sub_category_id',
+        'variation_id',
+        'variations',
+        'size_id',
+        'sizes',
+        'volume_id',
+        'volumes',
+        'packaging_id',
+        'packaging',
+        'cap_id',
+        'caps',
+
+    ];
+
+    protected $casts = [
+        'variations' => 'array',
+        'sizes' => 'array',
+        'volumes' => 'array',
+        'packaging' => 'array',
+        'caps' => 'array',
+    ];
+
+}
